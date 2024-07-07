064165f27f68d7f09507acecbc6ecd0eb1415f5b
Konrad Martinius <k.martinius@gmx.net>
2024-07-07 22:53:04 +0200

    fertige MazeViz-Datei

--- src/structs/MazeViz.jl
+++ src/structs/MazeViz.jl
@@ -1,41 +1,129 @@
 module MazeVizModule
 using ..MazeModule: Maze, MazeViz
+using ..NodeModule: Node
+using Match
 export visualize_maze
+
 
 function visualize_maze(maze::Maze)::MazeViz     #wir machen die Visualisierung mit ASCII-Symbolen, deswegen ist der Output ein String
 
     height, width = size(maze.nodes)                # nehme die Parameter aus dem struct Maze
-    ceiling_top = "┌"
-    ceiling_bottom = "└"
-    for i in 1:width
-        ceiling_top *= "--"
-        ceiling_bottom *= "--"
-    end
-    ceiling_top *= "┐\n"
-    ceiling_bottom *= "┘\n"
 
-    path_nodes = Set(maze.path)                 # für die Lösung
-    output = ceiling_top
+    # path_nodes = Set(maze.path)                 # für die Lösung
+    output_list = Matrix{Union{Nothing, String}}(nothing, 2*height + 1 , 2*width + 1)
+    output_list[1,1] = " ┌─"
+    output_list[2*height + 1, 1] = " └─"
+    output_list[1, 2*width + 1] = "─┐ "
+    output_list[2*height + 1 , 2*width + 1] = "─┘ "
+    output_list[1,:] .= "───"
+    output_list[2*height + 1,:] .= "───"
+    output_list[:,1] .= " | "
+    output_list[:,2*width + 1] .= " | "
+
     for i in 1:height
-        output *= "|"
         for j in 1:width
-            if maze.nodes[i, j] in path_nodes   # wenn das Pfad ist, dann anders markieren
-                output *= "* "                  # mit * nämlich
-            elseif maze.nodes[i, j] === nothing
-                output *= "--"                  # Wand
-            else
-
-                output *= "  "                  # Durchgang
+            output_list[2*i, 2*j] = "   "
+            node = maze.nodes[i, j]         # right, bottom, left, top
+            if node.neighbors[1] !== nothing
+                output_list[2*i, 2*j + 1] = "   "
+            end
+            if node.neighbors[2] !== nothing
+                output_list[2*i + 1, 2*j] = "   "
+            end
+            if node.neighbors[3] !== nothing
+                output_list[2*i, 2*j - 1] = "   "
+            end
+            if node.neighbors[4] !== nothing
+                output_list[2*i - 1, 2*j] = "   "
             end
         end
-        output *= "|\n"
     end
-    output *= ceiling_bottom
+
+    
+    for i in 1:2*height+1
+        for j in 1:2*width+1
+            if i % 2 == 0 && j % 2 == 0
+                output_list[i, j] = "   "
+                continue
+            end
+            if output_list[i, j] == "   "
+                continue
+            end
+            possible_neighbors = get_possible_neighbors(output_list, i, j)
+            output_list[i, j] = choose_right_edge(possible_neighbors)
+            
+        end
+    end
+
+    output_list[2 * maze.start.position[1], 2 * maze.start.position[2]] = " ⁕ "
+    output_list[2 * maze.goal.position[1], 2 * maze.goal.position[2]] = " ⛿ "
+
+    output = ""
+    for i in 1:2*height+1
+        for j in 1:2*width+1
+            output *= output_list[i, j]
+        end
+        output *= "\n"
+    end
 
     viz = MazeViz(output)
     return viz
 end
 
+
+function get_possible_neighbors(list::Matrix{Union{Nothing, String}}, i::Int, j::Int)::Vector{Int}
+    height, width = size(list)
+
+    possible_neighbors = [1,1,1,1]          
+    if i > 1
+        if list[i - 1, j] !== "   "  # top ceiling
+            possible_neighbors[4] = 0
+        end
+    end
+    if i < height
+        if list[i + 1, j] !== "   "  # bottom ceiling
+            possible_neighbors[2] = 0
+        end
+    end
+    if j > 1
+        if list[i, j - 1] !== "   "   # left ceiling
+            possible_neighbors[3] = 0
+        end
+    end
+    if j < width 
+        if list[i, j + 1] !== "   "     # right ceiling
+            possible_neighbors[1] = 0
+        end
+    end            
+
+    return possible_neighbors 
+end
+
+
+function choose_right_edge(possible_neighbors::Vector{Int})::String
+# ─ | ┌ └ ┐ ┘ ├ ┤ ┬ ┴ ╴ ╵ ╶ ╷
+# right, bottom, left, top
+    str = @match possible_neighbors begin
+        [0,0,0,0] => "─┼─"
+        [0,0,0,1] => "─┬─"
+        [0,0,1,0] => " ├─"
+        [0,0,1,1] => " ┌─"
+        [0,1,0,0] => "─┴─"
+        [0,1,0,1] => "───"
+        [0,1,1,0] => " └─"
+        [0,1,1,1] => "───"
+        [1,0,0,0] => "─┤ "
+        [1,0,0,1] => "─┐ "
+        [1,0,1,0] => " │ "
+        [1,0,1,1] => " │ "
+        [1,1,0,0] => "─┘ "
+        [1,1,0,1] => "── "
+        [1,1,1,0] => " │ "
+        [1,1,1,1] => "   "
+    end
+
+    return str    
+end
 
 function Base.show(io::IO, maze::Maze)
     viz = visualize_maze(maze)
