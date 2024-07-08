c6b907866a6231cd0f9cb77dcfe8199a23286ce1
Konrad Martinius <k.martinius@gmx.net>
2024-07-08 12:55:00 +0200

    Kommentare für Vorstellung in random_tree, MazeViz

--- src/random_tree.jl
+++ src/random_tree.jl
@@ -8,34 +8,38 @@ using ..MazeVizModule: visualize_maze
 export maze
 
 function maze(height::Int, width::Int)::Maze
-    # Das ist der eigentliche Konstruktor.
+    # Das ist der eigentliche Maze Konstruktor.
 
-    # Startet mit der Erzeugung der Matrix
+    # Startet mit der Erzeugung der leeren Matrix
     maze_matrix = Matrix{Union{Nothing,Node}}(nothing, height, width)
 
-    # Sucht erstmal eine randomizierte Wurzel
+    # Sucht erstmal eine zufällige Wurzel
     start = Node([rand(1:height), rand(1:width)], [nothing, nothing, nothing, nothing])
     maze_matrix[start.position[1], start.position[2]] = start
 
     # Ruft die rekursive Funktion für die Labyrinth Erstellung auf
-    maze_matrix = maze_matrix_recursive(maze_matrix, start)
+    maze_matrix = maze_matrix_recursive!(maze_matrix, start)
     goal = maze_matrix[rand(1:height), rand(1:width)]
     maze = Maze(maze_matrix, nothing, nothing, start, goal)
 
+    # Löst das Labyrinth und visualisiert es
     maze.path = solve(maze)
-
     viz = visualize_maze(maze)
-
     maze.visual = viz
 
     return maze
 end
 
-function maze_matrix_recursive(maze_matrix::Matrix{Union{Nothing,Node}}, node::Node)::Matrix{Union{Nothing,Node}}
-    # Macht eine rekursive Version der Tiefensuche
+function maze_matrix_recursive!(maze_matrix::Matrix{Union{Nothing,Node}}, node::Node)::Matrix{Union{Nothing,Node}}
+    # Schreibt eine rekursive Version der Tiefensuche für die Erstellung des Labyrinths
+    # Arguments:
+    #   maze_matrix:    Eine zu Beginn leere Matrix, mit der die Funktion in place arbeitet und die erstellten Knoten reinschreibt
+    #   node:           Der aktuelle Knoten an dem sich die Tiefensuche gerade befindet
+    # Gibt die maze_matrix zurück, wenn der aktuelle Knoten keine verfügbaren Nachbarn mehr hat
+
     next_node = set_next_node!(node, maze_matrix)
     while next_node !== nothing
-        maze_matrix = maze_matrix_recursive(maze_matrix, next_node)
+        maze_matrix = maze_matrix_recursive!(maze_matrix, next_node)
         next_node = set_next_node!(node, maze_matrix)
     end
 
@@ -44,24 +48,28 @@ function maze_matrix_recursive(maze_matrix::Matrix{Union{Nothing,Node}}, node::N
 end
 
 function available_neighbors(node::Node, maze_matrix::Matrix{Union{Nothing,Node}})
-    # Sucht mit der node und der Matrix alle möglichen Nachbarn. 
+    # Sucht mit der node und der Matrix alle möglichen Nachbarn. Also alle Knoten die noch nicht besucht wurden und dennoch existieren.
+    # Arguments:
+    #   node:           Der zu untersuchende Knoten
+    #   maze_matrix:    Die Matrix des Labyrinths
+    # Gibt die Liste der gefundenen Nachbarn zurück
     neighbors = []
-    if node.position[2] != 1
+    if node.position[2] != 1    # left
         if node.neighbors[3] === nothing && maze_matrix[node.position[1], (node.position[2]-1)] === nothing
             push!(neighbors, 'l')
         end
     end
-    if node.position[2] != size(maze_matrix, 2)
+    if node.position[2] != size(maze_matrix, 2) # right
         if node.neighbors[1] === nothing && maze_matrix[node.position[1], node.position[2]+1] === nothing
             push!(neighbors, 'r')
         end
     end
-    if node.position[1] != 1
+    if node.position[1] != 1    # top
         if node.neighbors[4] === nothing && maze_matrix[node.position[1]-1, node.position[2]] === nothing
             push!(neighbors, 't')
         end
     end
-    if node.position[1] != size(maze_matrix, 1)
+    if node.position[1] != size(maze_matrix, 1) # bottom
         if node.neighbors[2] === nothing && maze_matrix[node.position[1]+1, node.position[2]] === nothing
             push!(neighbors, 'b')
         end
@@ -70,7 +78,12 @@ function available_neighbors(node::Node, maze_matrix::Matrix{Union{Nothing,Node}
 end
 
 function set_next_node!(node::Node, maze_matrix::Matrix{Union{Nothing,Node}})::Union{Nothing,Node}
-    # Sucht mit available_neighbors alle verfügbaren Nachbarn und wählt dann einen Nachbarn zufallig aus. Passt die Matrix und die Node auch dementsprechend an. Gibt nothing zurück falls die node keine Nachbarn hat.
+    # Sucht mit available_neighbors alle verfügbaren Nachbarn und wählt dann einen Nachbarn zufallig aus. 
+    # Passt die Matrix und die Node auch dementsprechend an. Gibt nothing zurück falls die node keine verfügbare Nachbarn mehr hat.
+    # Arguments:
+    #   node:           Der zu untersuchende Knoten
+    #   maze_matrix:    Die Matrix des Labyrinths
+    # Verändert die maze_matrix in place und gibt den neuen ausgewählten KNoten zurück.
 
     neighbors = available_neighbors(node, maze_matrix)
 
--- src/structs/MazeViz.jl
+++ src/structs/MazeViz.jl
@@ -5,10 +5,15 @@ using Match
 export visualize_maze
 
 
-function visualize_maze(maze::Maze)::MazeViz     #wir machen die Visualisierung mit ASCII-Symbolen, deswegen ist der Output ein String
+function visualize_maze(maze::Maze)::MazeViz     
+    # Funktion für die Visualisierung des Labyrinths.
+    # Arguments:
+    #   maze:   Übergebenes Labyrinth das visualisiert werden sollene
+    # Gibt den MazeViz struct zurück.
 
-    height, width = size(maze.nodes)                # nehme die Parameter aus dem struct Maze
+    height, width = size(maze.nodes)                
     
+    # Eine Matrix für das visualisierte Labyrinth erstellen
     output_list = Matrix{Union{Nothing, String}}(nothing, 2*height + 1 , 2*width + 1)
     output_list[1,1] = " ┌─"
     output_list[2*height + 1, 1] = " └─"
@@ -19,12 +24,13 @@ function visualize_maze(maze::Maze)::MazeViz     #wir machen die Visualisierung 
     output_list[:,1] .= " | "
     output_list[:,2*width + 1] .= " | "
 
-    # Für die Lösung
+    # Für die Lösung eine Liste aus allen Koordinaten der Knoten, die für die Lösung notwendig sind.
     position_list = []
     for node in maze.path
         push!(position_list, (node.position))
     end
 
+    # Die maze_matrix und die Lösung in die output_list transferieren
     for i in 1:height
         for j in 1:width
             
@@ -78,7 +84,7 @@ function visualize_maze(maze::Maze)::MazeViz     #wir machen die Visualisierung 
         end
     end
 
-    
+    # Restliche Wände für die Darstellung auswählen
     for i in 1:2*height+1
         for j in 1:2*width+1
             if i % 2 == 0 && j % 2 == 0
@@ -93,16 +99,10 @@ function visualize_maze(maze::Maze)::MazeViz     #wir machen die Visualisierung 
         end
     end
     
-    for i in 1:height
-        for j in 1:width
-
-        end
-    end
     output_list[2 * maze.start.position[1], 2 * maze.start.position[2]] = " ⁕ "
     output_list[2 * maze.goal.position[1], 2 * maze.goal.position[2]] = " ⛿ "
 
-
-
+    # Output in einen String zusammenfassen 
     output = ""
     for i in 1:2*height+1
         for j in 1:2*width+1
@@ -117,26 +117,33 @@ end
 
 
 function get_possible_neighbors(list::Matrix{Union{Nothing, String}}, i::Int, j::Int)::Vector{Int}
+    # Erstellt eine codierte Liste aus allen möglichen Nachbarwänden, um das richtige Wandstück auszuwählen.
+    # Arguments:
+    #   list:   Um Wände erweiterte Labyrinth-Matrix
+    #   i:      Aktuelle Höhenkoordinate für die Liste
+    #   j:      Aktuelle Breitenkoordinate für die Liste
+    # Gibt einen kodierten Vektor mit 4 Einträgen zurück.
+
     height, width = size(list)
 
     possible_neighbors = [1,1,1,1]          
     if i > 1
-        if list[i - 1, j] !== "   " && list[i - 1, j] !== " + " # top ceiling
+        if list[i - 1, j] !== "   " && list[i - 1, j] !== " + " # top wall
             possible_neighbors[4] = 0
         end
     end
     if i < height
-        if list[i + 1, j] !== "   " && list[i + 1, j] !== " + "   # bottom ceiling
+        if list[i + 1, j] !== "   " && list[i + 1, j] !== " + "   # bottom wall
             possible_neighbors[2] = 0
         end
     end
     if j > 1
-        if list[i, j - 1] !== "   " && list[i, j - 1] !== " + "    # left ceiling
+        if list[i, j - 1] !== "   " && list[i, j - 1] !== " + "    # left wall
             possible_neighbors[3] = 0
         end
     end
     if j < width 
-        if list[i, j + 1] !== "   " && list[i, j + 1] !== " + "      # right ceiling
+        if list[i, j + 1] !== "   " && list[i, j + 1] !== " + "      # right wall
             possible_neighbors[1] = 0
         end
     end            
@@ -146,8 +153,15 @@ end
 
 
 function choose_right_edge(possible_neighbors::Vector{Int})::String
-# ─ | ┌ └ ┐ ┘ ├ ┤ ┬ ┴ ╴ ╵ ╶ ╷
-# right, bottom, left, top
+    # Wählt aus einem codierten Vektor das richtige Wandelement aus.
+    # Zu wählende Wandelemente:  ─ | ┌ └ ┐ ┘ ├ ┤ ┬ ┴ ╴ ╵ ╶ ╷
+    # Arguments:
+    #   possible_neighbors: Codierter Vektor bestehend aus 1 und 0. Jeder Wert steht für eine mögliche Richtung.
+    #                       Ist in folgender Reihenfolge codiert: right, bottom, left, top 
+    #                       1: Zur entsprechenden Richtung ist keine Wandverbindung notwendig
+    #                       0: Zur entsprechenden Richtung ist eine Wandverbindung notwendig
+    # Gibt den ausgewählten String zurück
+
     str = @match possible_neighbors begin
         [0,0,0,0] => "─┼─"
         [0,0,0,1] => "─┬─"
@@ -156,13 +170,13 @@ function choose_right_edge(possible_neighbors::Vector{Int})::String
         [0,1,0,0] => "─┴─"
         [0,1,0,1] => "───"
         [0,1,1,0] => " └─"
-        [0,1,1,1] => "───"
+        [0,1,1,1] => " ╶─"
         [1,0,0,0] => "─┤ "
         [1,0,0,1] => "─┐ "
         [1,0,1,0] => " │ "
         [1,0,1,1] => " ╷ "
         [1,1,0,0] => "─┘ "
-        [1,1,0,1] => "── "
+        [1,1,0,1] => "─╴ "
         [1,1,1,0] => " ╵ "
         [1,1,1,1] => "   "
     end
@@ -170,11 +184,10 @@ function choose_right_edge(possible_neighbors::Vector{Int})::String
     return str    
 end
 
-function show_path(path::Vector{Node}, list::Matrix{Union{Nothing, String}})
-    print()
-end
 
 function Base.show(io::IO, maze::Maze)
+    # Überläd die Darstellung für das Labyrinth mit der erstellten Visualisierung.
+    
     viz = visualize_maze(maze)
     print(io, viz.visual)
 end
